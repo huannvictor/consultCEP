f2d08195763a4055ffcbafb68a4da8f3b301f2cf
Huann Almeida <67878933+huannvictor@users.noreply.github.com>
2021-09-30 15:49:18 -0300

    rendered data

--- app.js
+++ app.js
@@ -16,9 +16,21 @@ function run(event){
     axios
         .get('https://viacep.com.br/ws/'+cepCode+'/json/')
         .then(function(response){
-            console.log(response.data)
+            content.innerHTML = ''
+            createLine(response.data.logradouro)
+            createLine(response.data.bairro)
+            createLine(response.data.localidade +'/'+ response.data.uf)
         })
         .catch(function(error){
             console.log(error)
         })
+}
+
+function createLine(text){
+    var line = document.createElement('p')
+    var text = document.createTextNode(text)
+    
+    line.appendChild(text)
+    content.appendChild(line)
+    
 }
